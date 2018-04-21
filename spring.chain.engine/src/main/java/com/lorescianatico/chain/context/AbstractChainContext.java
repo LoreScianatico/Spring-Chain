6207962e9f405c3b018c2ef2d6444854aebdfd91
Lorenzo <scianaticolorenzo@gmail.com>
2018-04-21 17:24:41 +0200

    Adding abstract context class.

--- spring.chain.engine/src/main/java/com/lorescianatico/chain/context/AbstractChainContext.java
+++ spring.chain.engine/src/main/java/com/lorescianatico/chain/context/AbstractChainContext.java
@@ -1,7 +1,15 @@
 package com.lorescianatico.chain.context;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Abstract Context Class for chain
  */
+@Getter
+@Setter
 public abstract class AbstractChainContext {
+
+    private String lastExecutedHandler;
+
 }
