02e2605590903fd96ef4c99a758297da7a59f9da
Lorenzo <scianaticolorenzo@gmail.com>
2018-11-10 17:35:15 +0100

    Minor improvements.

--- spring.chain.engine/src/main/java/com/lorescianatico/chain/configuration/CatalogReader.java
+++ spring.chain.engine/src/main/java/com/lorescianatico/chain/configuration/CatalogReader.java
@@ -4,6 +4,8 @@ import com.lorescianatico.chain.configuration.model.Catalog;
 import com.lorescianatico.chain.fault.InvalidCatalogException;
 import lombok.Synchronized;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
@@ -18,7 +20,6 @@ import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -31,12 +32,13 @@ public final class CatalogReader {
     /**
      * Schema definition
      */
-    private static final String SCHEMA = "/catalog.xsd";
+    private static final String SCHEMA = "catalog.xsd";
 
     /**
      * Private constructor
      */
-    private CatalogReader() {}
+    private CatalogReader() {
+    }
 
     /**
      * Get the catalog configuration
@@ -45,24 +47,25 @@ public final class CatalogReader {
      * @return the deserialized catalog object
      */
     @Synchronized
-    public static Catalog getCatalog(String sourceFile){
+    public static Catalog getCatalog(String sourceFile) {
         verifyCatalog(sourceFile);
         return loadCatalog(sourceFile);
     }
 
     /**
      * Verify that catalog configuration is compliant with the catalog schema
+     *
      * @param sourceFile the catalog to be verified
      */
     private static void verifyCatalog(String sourceFile) {
         logger.debug("Validating catalog file: {}", sourceFile);
-        try (InputStream xml = new FileInputStream(sourceFile)) {
-            InputStream xsd = CatalogReader.class.getResourceAsStream(SCHEMA);
-            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = factory.newSchema(new StreamSource(xsd));
-            Validator validator = schema.newValidator();
-            validator.validate(new StreamSource(xml));
-            logger.debug("Catalog file is valid.");
+        try (InputStream xml = new FileSystemResource(sourceFile).getInputStream();
+             InputStream xsd = new ClassPathResource(SCHEMA).getInputStream()) {
+             SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+             Schema schema = factory.newSchema(new StreamSource(xsd));
+             Validator validator = schema.newValidator();
+             validator.validate(new StreamSource(xml));
+             logger.debug("Catalog file is valid.");
         } catch (SAXException | IOException e) {
             logger.error("Error while validating configuration: {}", e.getMessage());
             throw new InvalidCatalogException("Error while validating configuration: " + e.getMessage());
@@ -71,22 +74,23 @@ public final class CatalogReader {
     }
 
     /**
-     * Loads catalog from source fils
+     * Loads catalog from source file
+     *
      * @param sourceFile the catalog source
      * @return Deserialized catalog
      */
     private static Catalog loadCatalog(String sourceFile) {
         logger.debug("Reading catalog: {}", sourceFile);
-        try (InputStream xml = new FileInputStream(sourceFile)){
+        try (InputStream xml = new FileSystemResource(sourceFile).getInputStream()) {
             JAXBContext jaxbContext = JAXBContext.newInstance(Catalog.class.getPackage().getName());
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
             XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
             xmlInputFactory.setProperty("javax.xml.stream.supportDTD", false);
-	    xmlInputFactory.setProperty("javax.xml.stream.isReplacingEntityReferences", false);
-	    xmlInputFactory.setProperty("javax.xml.stream.isSupportingExternalEntities", false);
+            xmlInputFactory.setProperty("javax.xml.stream.isReplacingEntityReferences", false);
+            xmlInputFactory.setProperty("javax.xml.stream.isSupportingExternalEntities", false);
             XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(xml);
             JAXBElement<Catalog> catalogDeserialized = jaxbUnmarshaller.unmarshal(xmlEventReader, Catalog.class);
-            return  catalogDeserialized.getValue();
+            return catalogDeserialized.getValue();
         } catch (JAXBException | XMLStreamException e) {
             logger.error("Error while unmarshalling catalog: {}", e.getMessage());
             throw new InvalidCatalogException("Error while unmarshalling catalog: " + e.getMessage());
