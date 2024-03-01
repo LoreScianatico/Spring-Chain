d62ba32c70c3109da1590d0214470386a27e2f41
e514001 <lorenzo.scianatico@sbb.ch>
2024-03-01 09:34:00 +0100

    Efficient ingredient query

--- spring.chain.poc/src/main/java/com/lorescianatico/spring/chain/chain/IngredientMapperHandler.java
+++ spring.chain.poc/src/main/java/com/lorescianatico/spring/chain/chain/IngredientMapperHandler.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 
 import java.util.List;
-import java.util.Set;
 
 @ChainHandler
 @Slf4j
